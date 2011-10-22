9ac3846c7404709cb2deb083f7c7034e9910c2d0
markrendle <mark@markrendle.net>
2011-10-22 19:35:19 +0100

    Added models and data access

---
+++ src/GiveCRM.Admin.DataAccess/Charities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GiveCRM.Admin.Models;
+using Simple.Data;
+
+namespace GiveCRM.Admin.DataAccess
+{
+    public class Charities
+    {
+        private readonly dynamic _db = Database.OpenNamedConnection("GiveCRMAdmin");
+
+        public Charity Get(int id)
+        {
+            return _db.Charity.FindById(id);
+        }
+
+        public IEnumerable<Charity> All()
+        {
+            return _db.Charity.All().Cast<Charity>();
+        }
+
+        public Charity GetByUserId(string userId)
+        {
+            return _db.Charity.FindByUserId(userId);
+        }
+
+        public Charity Insert(Charity charity)
+        {
+            return _db.Charity.Insert(charity);
+        }
+
+        public void Update(Charity charity)
+        {
+            _db.Charity.UpdateById(charity);
+        }
+
+        public void Delete(Charity charity)
+        {
+            _db.Charity.DeleteById(charity.Id);
+        }
+    }
+}
---
+++ src/GiveCRM.Admin.Database/Schema Objects/Schemas/dbo/Tables/Charity.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Charity]
+(
+	Id int IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+	Name nvarchar(100) NOT NULL,
+	UserName nvarchar(100) NULL,
+	RegisteredCharityNumber nvarchar(20) NULL,
+	SubDomain nvarchar(20) NULL,
+	UserId nvarchar(50) NOT NULL,
+	EncryptedPassword binary(64) NOT NULL,
+	Salt binary(16) NOT NULL
+)
