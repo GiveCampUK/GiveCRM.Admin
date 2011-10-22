29cf6528db0859b446ba273e271fe295bc422eb2
robinminto <robin@minto.co.uk>
2011-10-22 15:01:25 +0100

    Added SignUp controller

---
+++ src/GiveCRM.Admin.Web/Controllers/SignUpController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace GiveCRM.Admin.Web.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return RedirectToAction("Complete");
+        }
+
+        public ActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
