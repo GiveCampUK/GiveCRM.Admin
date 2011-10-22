879d757b617a72b48c7e56bf55bd95bd98295821
robinminto <robin@minto.co.uk>
2011-10-22 17:09:43 +0100

    Derive sub-domain from charity name

--- src/GiveCRM.Admin.Web/Controllers/SignUpController.cs
+++ src/GiveCRM.Admin.Web/Controllers/SignUpController.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 using GiveCRM.Admin.Web.Interfaces;
 using GiveCRM.Admin.Web.ViewModels.SignUp;
@@ -31,16 +33,19 @@ namespace GiveCRM.Admin.Web.Controllers
             Add membership record
             Queue activation email sending
             Queue provisioning 
-            */ 
+            */
+            TempData["CharityName"] = requiredInfo.CharityName;
 
             return RedirectToAction("Complete");
         }
 
         public ActionResult Complete()
         {
+            var charityName = TempData["CharityName"] as string;
+
             var additionalInfo = new AdditionalInfo
                                 {
-                                    SubDomain = "mycharity"
+                                    SubDomain = GetSubDomainFromCharityName(charityName)
                                 };
             var viewModel = new Complete
                                 {
@@ -49,6 +54,12 @@ namespace GiveCRM.Admin.Web.Controllers
                                 };
 
             return View(viewModel);
+        }
+
+        private static string GetSubDomainFromCharityName(string charityName)
+        {
+            var result = Regex.Replace(charityName, @"[\s]", "-");
+            return Regex.Replace(result, @"[^\w-]", "");
         }
     }
 }
