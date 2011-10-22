cd7b49f74eaa7e9631bd508c823debbeccb3f7e4
robinminto <robin@minto.co.uk>
2011-10-22 11:23:30 +0100

    Basic sign up form and model

---
+++ GiveCRM.Admin.Models/SignUp.cs
@@ -0,0 +1,10 @@
+﻿namespace GiveCRM.Admin.Models
+{
+    public class SignUp
+    {
+        public string UserIdentifier { get; set; }
+        public string Password { get; set; }
+        public string CharityName { get; set; }
+        public bool TermsAccepted { get; set; }
+    }
+}
