8d042d6102c0698f698967b5f5d9e9ff0b4708e1
Alastair Smith <github@alastairsmith.me.uk>
2011-10-22 18:45:56 +0100

    Pass some more unit tests.

--- src/GiveCRM.Admin.Web.Tests/ExcelImportController_Upload_Should.cs
+++ src/GiveCRM.Admin.Web.Tests/ExcelImportController_Upload_Should.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web;
-using System.Web.Routing;
 using GiveCRM.Admin.Web.Controllers;
+using GiveCRM.Admin.Web.Interfaces;
+using GiveCRM.ImportExport;
 using Moq;
 using NUnit.Framework;
 using System.Web.Mvc;
@@ -11,18 +12,26 @@ namespace GiveCRM.Admin.Web.Tests
     public class ExcelImportController_Upload_Should
     {
         private ExcelImportController controller;
+        
+        private const string DashboardControllerName = "Dashboard";
+        private const string IndexActionName = "Index";
+
+        private const int ZeroFileLength = 0;
+        private const int NonZeroFileLength = 4096;
 
         [SetUp]
         public void SetUp()
         {
-            controller = new ExcelImportController();
+            var fileSystem = new Mock<IFileSystemWrapper>();
+            var excelImporter = new Mock<IExcelImport>();
+            controller = new ExcelImportController(excelImporter.Object, fileSystem.Object);
         }
 
         [Test]
         public void ReturnToIndexView_WhenNoFileIsSelectedForUpload()
         {
             var result = controller.Upload(null) as ViewResult;
-            Assert.AreEqual("Index", result.ViewName);
+            Assert.AreEqual(IndexActionName, result.ViewName);
         }
 
         [Test]
@@ -36,18 +45,18 @@ namespace GiveCRM.Admin.Web.Tests
         public void ReturnToIndexView_WhenAnEmptyFileIsUploaded()
         {
             var file = new Mock<HttpPostedFileBase>();
-            file.Setup(f => f.ContentLength).Returns(0);
+            file.Setup(f => f.ContentLength).Returns(ZeroFileLength);
 
             var result = controller.Upload(file.Object) as ViewResult;
 
-            Assert.AreEqual("Index", result.ViewName);
+            Assert.AreEqual(IndexActionName, result.ViewName);
         }
 
         [Test]
         public void DisplayAnErrorMessage_WhenAnEmptyFileIsUploaded()
         {
             var file = new Mock<HttpPostedFileBase>();
-            file.Setup(f => f.ContentLength).Returns(0);
+            file.Setup(f => f.ContentLength).Returns(ZeroFileLength);
 
             var result = controller.Upload(file.Object) as ViewResult;
 
@@ -55,15 +64,64 @@ namespace GiveCRM.Admin.Web.Tests
         }
 
         [Test]
+        public void ReturnToIndexView_WhenANonExcelFileIsUploaded()
+        {
+            var file = new Mock<HttpPostedFileBase>();
+            file.Setup(f => f.ContentLength).Returns(NonZeroFileLength);
+            file.Setup(f => f.FileName).Returns("A Text File.txt");
+
+            var result = controller.Upload(file.Object) as ViewResult;
+
+            Assert.AreEqual(IndexActionName, result.ViewName);
+        }
+
+        [Test]
+        public void DisplayAnErrorMessage_WhenANonExcelFileIsUploaded()
+        {
+            var file = new Mock<HttpPostedFileBase>();
+            file.Setup(f => f.ContentLength).Returns(NonZeroFileLength);
+            file.Setup(f => f.FileName).Returns("A Text File.txt");
+
+            var result = controller.Upload(file.Object) as ViewResult;
+
+            Assert.IsNotNull(result.ViewBag.Error);
+        }
+
+        [Test]
+        public void NotReturnAViewResult_WhenAnExcelFileIsUploaded_OldFormat()
+        {
+            var file = new Mock<HttpPostedFileBase>();
+            file.Setup(f => f.ContentLength).Returns(NonZeroFileLength);
+            file.Setup(f => f.FileName).Returns("An old-format Excel file.xls");
+
+            var result = controller.Upload(file.Object);
+
+            Assert.IsNotInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void NotReturnAViewResult_WhenAnExcelFileIsUploaded_NewFormat()
+        {
+            var file = new Mock<HttpPostedFileBase>();
+            file.Setup(f => f.ContentLength).Returns(NonZeroFileLength);
+            file.Setup(f => f.FileName).Returns("A new-format Excel file.xlsx");
+
+            var result = controller.Upload(file.Object);
+
+            Assert.IsNotInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
         public void RedirectToDashboardIndex_WhenFileIsValidForImport()
         {
             var file = new Mock<HttpPostedFileBase>();
-            file.Setup(f => f.ContentLength).Returns(4096);
+            file.Setup(f => f.ContentLength).Returns(NonZeroFileLength);
+            file.Setup(f => f.FileName).Returns("Valid file.xls");
 
             var result = controller.Upload(file.Object) as RedirectToRouteResult;
-            
-            Assert.AreEqual("Dashboard", result.RouteValues["controller"]);
-            Assert.AreEqual("Index", result.RouteValues["action"]);
+
+            Assert.AreEqual(DashboardControllerName, result.RouteValues["controller"]);
+            Assert.AreEqual(IndexActionName, result.RouteValues["action"]);
         }
     }
 }
--- src/GiveCRM.Admin.Web/Controllers/ExcelImportController.cs
+++ src/GiveCRM.Admin.Web/Controllers/ExcelImportController.cs
@@ -1,11 +1,37 @@
-﻿using System.Web;
+﻿using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
+using GiveCRM.Admin.Web.Interfaces;
 using GiveCRM.Admin.Web.ViewModels;
+using GiveCRM.ImportExport;
 
 namespace GiveCRM.Admin.Web.Controllers
 {
-    public class ExcelImportController : Controller
+    public class ExcelImportController : AsyncController
     {
+        private const string ExcelFileExtension_OldFormat = ".xls";
+        private const string ExcelFileExtension_NewFormat = ".xlsx";
+
+        private IFileSystemWrapper fileSystemWrapper;
+        private IExcelImport excelImporter;
+
+        public ExcelImportController(IExcelImport excelImporter, IFileSystemWrapper fileSystemWrapper)
+        {
+            if (excelImporter == null)
+            {
+                throw new ArgumentNullException("excelImporter");
+            }
+
+            if (fileSystemWrapper == null)
+            {
+                throw new ArgumentNullException("fileSystemWrapper");
+            }
+
+            this.excelImporter = excelImporter;
+            this.fileSystemWrapper = fileSystemWrapper;
+        }
+
         public ActionResult Index()
         {
            return View(new ExcelImportViewModel());
@@ -16,19 +42,44 @@ namespace GiveCRM.Admin.Web.Controllers
         {
             if (file == null)
             {
-                ViewBag.Error = "You did not select a file for upload.";
-                return View("Index", new ExcelImportViewModel());
+                return ErrorView("You did not select a file for upload.");
             }
-            
+
             if (file.ContentLength <= 0)
             {
-                ViewBag.Error = "The file you uploaded was empty.";
-                return View("Index", new ExcelImportViewModel());
+                return ErrorView("The file you uploaded was empty.");
+            }
+
+            if (!IsValidFileExtension(file.FileName))
+            {
+                return ErrorView("The file you uploaded does not appear to be an Excel file.");
             }
 
             // Process the file
+            BeginImportAsync(file.FileName);
 
             return RedirectToAction("Index", "Dashboard");
+        }
+
+        private ActionResult ErrorView(string message)
+        {
+            ViewBag.Error = message;
+            return View("Index", new ExcelImportViewModel());
+        }
+
+        private bool IsValidFileExtension(string fileName)
+        {
+            return fileName.EndsWith(ExcelFileExtension_OldFormat) || fileName.EndsWith(ExcelFileExtension_NewFormat);
+        }
+
+        private void BeginImportAsync(string fileName)
+        {
+            AsyncManager.OutstandingOperations.Increment();
+
+            Stream fileStream = fileSystemWrapper.Open(fileName, FileMode.Open, FileAccess.Read);
+            
+            excelImporter.Open(fileStream, ExcelFileType.XLS, true);
+            excelImporter.GetRowsAsKeyValuePairs(0); //Simple.Data
         }
     }
 }
