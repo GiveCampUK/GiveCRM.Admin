75ff491a734bfaee88feefc73608a6d987f47793
robinminto <robin@minto.co.uk>
2011-10-22 13:51:50 +0100

    Added basic layout and sign up form

---
+++ src/GiveCRM.Admin.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GiveCRM.Admin.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
