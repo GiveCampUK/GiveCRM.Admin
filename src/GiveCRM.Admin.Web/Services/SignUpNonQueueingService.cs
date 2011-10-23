12a8d5d25091312a30f13d4d596d32b14d84cdca
robinminto <robin@minto.co.uk>
2011-10-23 10:18:15 +0100

    Pass domain from configuration to email template

--- src/GiveCRM.Admin.Web/Services/SignUpNonQueueingService.cs
+++ src/GiveCRM.Admin.Web/Services/SignUpNonQueueingService.cs
@@ -6,11 +6,19 @@ namespace GiveCRM.Admin.Web.Services
 {
     public class SignUpNonQueueingService : ISignUpQueueingService
     {
+        private readonly IConfiguration configuration;
+
+        public SignUpNonQueueingService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void QueueEmail(EmailViewModel emailViewModel)
         {
             dynamic email = new Email("SignUp");
             email.To = emailViewModel.To;
             email.ActivationToken = emailViewModel.ActivationToken;
+            email.BaseDomain = configuration.BaseDomain;
             email.Send();
         }
 
