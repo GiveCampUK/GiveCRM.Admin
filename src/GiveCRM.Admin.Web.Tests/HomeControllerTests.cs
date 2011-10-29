b7f0151cb99825f4c6913278eae13ffc0e32ea20
robinminto <robin@minto.co.uk>
2011-10-30 00:13:54 +0100

    Basic controller tests

---
+++ src/GiveCRM.Admin.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using GiveCRM.Admin.Web.Controllers;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+
+namespace GiveCRM.Admin.Web.Tests
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index_ReturnsView()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+
+            result.AssertViewRendered();
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
