2601462e31076445b824724d7da9fd681314f901
robinminto <robin@minto.co.uk>
2011-10-29 16:49:19 +0100

    Changed database name (. is unhelpful) and changed DB deployment project to deploy locally, without dropping existing database

--- src/GiveCRM.Admin.Database/Schema Objects/Database Level Objects/Storage/Files/GiveCRM.Admin.Database_log.sqlfile.sql
+++
@@ -1,3 +0,0 @@
-﻿ALTER DATABASE [$(DatabaseName)]
-    ADD LOG FILE (NAME = [GiveCRM.Admin.Database_log], FILENAME = 'D:\SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\GiveCRM.Admin.Database_log.LDF', SIZE = 832 KB, MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
-
