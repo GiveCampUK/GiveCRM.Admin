10a3b300e6360062ed843c7a6554c1c60d3d368f
Alastair Smith <github@alastairsmith.me.uk>
2012-03-31 13:06:13 +0100

    Add TestFixtures for CharitiesMemberships.
    Tests are still to be implemented. Includes changes to the TestDB to add a
    CharitiesMemberships table.

---
+++ src/GiveCRM.Admin.DataAccess.Test/CharitiesMembershipsFacts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Simple.Data;
+
+namespace GiveCRM.Admin.DataAccess.Test
+{
+    [TestFixture]
+    public class CharitiesMembershipRepositoryFacts
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Database.OpenNamedConnection("GiveCRMAdmin").Charities.DeleteAll();
+        }
+
+        [TestFixture]
+        public class GetAllShould
+        {
+            [Test]
+            public void ReturnAnEmptyCollection_WhenThereAreNoCharityMembershipsDefined()
+            {
+                
+            }
+        }
+
+        [TestFixture]
+        public class GetByIdShould
+        {
+            
+        }
+
+        [TestFixture]
+        public class SaveShould
+        {
+
+        }
+
+        [TestFixture]
+        public class DeleteShould
+        {
+
+        }
+
+        [TestFixture]
+        public class DeleteByIdShould
+        {
+
+        }
+    }
+}
