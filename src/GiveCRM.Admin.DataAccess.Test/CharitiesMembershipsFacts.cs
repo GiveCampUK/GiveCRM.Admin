52b266289f153bf23a8a6e971319ce1bf2a03560
Alastair Smith <github@alastairsmith.me.uk>
2012-03-31 14:06:44 +0100

    Update tests.
    Simple.Data is upgraded to v0.16 in all projects; this is the most recent
    version available on NuGet).
    
    The CharityMembership table in the TestDB is modified so that the Id
    column is defined as an Identity column.
    
    The GetAll() tests for CharitiesMemberships are now fully implemented and
    passing.

--- src/GiveCRM.Admin.DataAccess.Test/CharitiesMembershipsFacts.cs
+++ src/GiveCRM.Admin.DataAccess.Test/CharitiesMembershipsFacts.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GiveCRM.Admin.Models;
 using NUnit.Framework;
 using Simple.Data;
 
@@ -10,20 +11,37 @@ namespace GiveCRM.Admin.DataAccess.Test
     [TestFixture]
     public class CharitiesMembershipRepositoryFacts
     {
-        [SetUp]
-        public void SetUp()
-        {
-            Database.OpenFile("TestDB.sdf").CharitiesMemberships.DeleteAll();
-        }
-
         [TestFixture]
         public class GetAllShould
         {
+            private readonly dynamic db = Database.OpenFile("TestDB.sdf");
+            [SetUp]
+            public void SetUp()
+            {
+                db.CharityMembership.DeleteAll();
+            }
+
             [Test]
             public void ReturnAnEmptyCollection_WhenThereAreNoCharityMembershipsDefined()
             {
-                var allCharitiesMemberships = new CharitiesMemberships().GetAll();
+                var allCharitiesMemberships = new CharitiesMemberships(db).GetAll();
                 Assert.That(allCharitiesMemberships, Is.Empty);
+            }
+
+            [Test]
+            public void ReturnACollectionOfItems_WhenThereAreCharityMembershipsDefined()
+            {
+                var charityMembership = new CharityMembership
+                                            {
+                                                Id = 1,
+                                                CharityId = 1,
+                                                UserName = "test"
+                                            };
+                var charitiesMemberships = new CharitiesMemberships(db);
+                charitiesMemberships.Save(charityMembership);
+
+                var allCharitiesMemberships = charitiesMemberships.GetAll();
+                Assert.That(allCharitiesMemberships.Single().Id, Is.EqualTo(charityMembership.Id));
             }
         }
 
