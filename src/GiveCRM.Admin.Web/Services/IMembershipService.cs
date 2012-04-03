5355585eb4b3a8138c284925f46ad3d667c0d7ad
Alastair Smith <github@alastairsmith.me.uk>
2012-04-03 22:55:20 +0100

    Add documentation to some of the MembershipProvider wrapper bits.

--- src/GiveCRM.Admin.Web/Services/IMembershipService.cs
+++ src/GiveCRM.Admin.Web/Services/IMembershipService.cs
@@ -1,8 +1,27 @@
 ﻿namespace GiveCRM.Admin.Web.Services
 {
+    /// <summary>
+    /// Simple interface to make controllers dealing with Membership more testable.
+    /// </summary>
     public interface IMembershipService
     {
+        /// <summary>
+        /// Get a user account registered against the provided username.
+        /// </summary>
+        /// <param name="username">The username of the user to fetch.</param>
+        /// <returns>If the username is registered to a user, then an <see cref="IMember" /> representing 
+        /// the user is returned; otherwise, <c>null</c> is returned</returns>
         IMember GetUser(string username);
+
+        /// <summary>
+        /// Registers a new user account with the provided username, password and email address.
+        /// </summary>
+        /// <param name="username">The username to register on the new user account.</param>
+        /// <param name="password">The password to register on the new user account.</param>
+        /// <param name="email">The email address to register on the new user account.</param>
+        /// <returns>A <see cref="UserCreationResult"/> value indicating whether or not the 
+        /// user account was created successfully.  If it was not created successfully, the 
+        /// specific reason for the failure is returned.</returns>
         UserCreationResult CreateUser(string username, string password, string email);
     }
 }
--- src/GiveCRM.Admin.Web/Services/UserCreationResult.cs
+++ src/GiveCRM.Admin.Web/Services/UserCreationResult.cs
@@ -1,12 +1,44 @@
+using System.Web.Security;
+
 namespace GiveCRM.Admin.Web.Services
 {
+    /// <summary>
+    /// Describes the various results that can be returned when creating a user account.  Has a loose
+    /// associating with <see cref="MembershipCreateStatus"/>
+    /// </summary>
     public enum UserCreationResult
     {
+        /// <summary>
+        /// The user account was created successfully.
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// The email address provided is already registered.
+        /// </summary>
         DuplicateEmail,
+
+        /// <summary>
+        /// The username provided is already registered.
+        /// </summary>
         DuplicateUsername,
+
+        /// <summary>
+        /// The email address provided is not valid.  For example, it might
+        /// be missing the @ symbol.
+        /// </summary>
         InvalidEmail,
+
+        /// <summary>
+        /// The username provided is not valid.  For example, it might contain
+        /// whitespace.
+        /// </summary>
         InvalidUsername,
+
+        /// <summary>
+        /// The password provided is not valid.  For example, it might only
+        /// contain lowercase letters.
+        /// </summary>
         InvalidPassword
     }
 }
